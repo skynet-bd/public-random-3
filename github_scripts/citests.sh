74b95bca7d9aac811f9bc5e1751aefdd0a3ce0ca
bdeshi <bdeshi@bdeshi.space>
2025-02-26 02:08:02 +0600

    add code

---
+++ github_scripts/citests.sh
@@ -0,0 +1,151 @@
+#!/bin/bash -xe
+#
+# Copyright (C) 2024, University of Nebraska-Lincoln
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License.  You may
+# obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+cp pelican stashcp
+cp pelican stash_plugin
+
+to_exit=0
+./stashcp -d osdf:///ospool/uc-shared/public/OSG-Staff/validation/test.txt ./query1
+rm query1
+
+# Test the plugin interface
+classad_output=$(./stash_plugin -classad)
+
+if ! [[ $classad_output =~ "PluginType = \"FileTransfer\"" ]]; then
+  echo "PluginType not in classad output"
+  to_exit=1
+fi
+
+if ! [[ $classad_output =~ "SupportedMethods = \"stash, osdf, pelican\"" ]]; then
+  echo "SupportedMethods not in classad output"
+  to_exit=1
+fi
+
+plugin_output=$(./stash_plugin osdf:///ospool/uc-shared/public/OSG-Staff/validation/test.txt query1)
+rm query1
+
+# shellcheck disable=SC2076
+if ! [[ $plugin_output =~ "TransferUrl = \"osdf:///ospool/uc-shared/public/OSG-Staff/validation/test.txt\"" ]]; then
+  echo "TransferUrl not in plugin output"
+  to_exit=1
+fi
+
+if ! [[ $plugin_output =~ "TransferSuccess = true" ]]; then
+  echo "TransferSuccess not in plugin output"
+  to_exit=1
+fi
+
+cat > infile <<EOF
+[ LocalFileName = "$PWD/query1"; Url = "osdf:///ospool/uc-shared/public/OSG-Staff/validation/test.txt" ]
+EOF
+
+./stash_plugin -infile $PWD/infile -outfile $PWD/outfile
+
+
+#####################################
+## Test LocalCache in front of OSDF #
+#####################################
+SOCKET_DIR="`mktemp -d -t pelican-citest-XXXXXX`"
+export PELICAN_LOCALCACHE_SOCKET=$SOCKET_DIR/socket
+export PELICAN_LOCALCACHE_DATALOCATION=$SOCKET_DIR/data
+export PELICAN_SERVER_ENABLEUI=false
+export PELICAN_TLSSKIPVERIFY=true
+
+./pelican serve -d -f osg-htc.org --module localcache &
+PELICAN_PID=$!
+
+cleanup() {
+    rm -rf -- "$SOCKET_DIR"
+    kill $PELICAN_PID
+    wait $PELICAN_PID || :
+}
+trap cleanup EXIT
+
+for _idx in {1..20}; do
+  if [ -e "$SOCKET_DIR/socket" ]; then
+    break
+  fi
+  sleep 0.3
+done
+if [ ! -e "$SOCKET_DIR/socket" ]; then
+  echo "pelican serve never dropped localcache socket"
+  exit 1
+fi
+
+
+NEAREST_CACHE="unix://$SOCKET_DIR/socket" ./stash_plugin -d osdf:///ospool/uc-shared/public/OSG-Staff/validation/test.txt /dev/null
+exit_status=$?
+
+if ! [[ "$exit_status" = 0 ]]; then
+  echo "Cache plugin download failed"
+  exit 1
+fi
+
+if [ ! -e "$SOCKET_DIR/data/ospool/uc-shared/public/OSG-Staff/validation/test.txt.DONE" ]; then
+  echo "Test file not in local cache"
+  exit 1
+fi
+
+# Test we work with PELICAN_NEAREST_CACHE as well
+PELICAN_NEAREST_CACHE="unix://$SOCKET_DIR/socket" ./stash_plugin -d osdf:///ospool/uc-shared/public/OSG-Staff/validation/test.txt /dev/null
+exit_status=$?
+
+if ! [[ "$exit_status" = 0 ]]; then
+  echo "Cache plugin download failed"
+  exit 1
+fi
+
+if [ ! -e "$SOCKET_DIR/data/ospool/uc-shared/public/OSG-Staff/validation/test.txt.DONE" ]; then
+  echo "Test file not in local cache"
+  exit 1
+fi
+
+########################################
+# Test we return 0 when HOME is not set
+########################################
+OLDHOME=$HOME
+unset HOME
+./stash_plugin -classad
+exit_status=$?
+
+if ! [[ "$exit_status" = 0 ]]; then
+  echo "Plugin did not return 0 when HOME is not set"
+  to_exit=1
+fi
+
+export HOME=$OLDHOME
+
+# Test we return 0 when HOME points to a nonwritable directory
+OLDHOME=$HOME
+unset HOME
+mkdir unwritable_dir
+chmod u-w,a-w unwritable_dir
+export HOME=unwriteable_dir
+
+./stash_plugin -classad
+exit_status=$?
+
+if ! [[ "$exit_status" = 0 ]]; then
+  echo "Plugin did not return 0 when HOME is set to an unwritable dir"
+  to_exit=1
+fi
+
+unset HOME
+export HOME=$OLDHOME
+rm -r unwritable_dir
+
+exit $to_exit
---
+++ github_scripts/stat_test.sh
@@ -0,0 +1,141 @@
+#!/bin/bash -xe
+
+# Copyright (C) 2024, Pelican Project, Morgridge Institute for Research
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you
+# may not use this file except in compliance with the License.  You may
+# obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This tests the functionality of director's stat call to query origins for the availability of an object
+
+set -e
+
+mkdir -p /tmp/pelican-test/stat_test
+
+mkdir -p /tmp/pelican-test/stat_test/origin
+chmod 777 /tmp/pelican-test/stat_test/origin
+
+# Setup env variables needed
+export PELICAN_FEDERATION_DIRECTORURL="https://$HOSTNAME:8444"
+export PELICAN_FEDERATION_REGISTRYURL="https://$HOSTNAME:8444"
+export PELICAN_TLSSKIPVERIFY=true
+export PELICAN_SERVER_ENABLEUI=false
+export PELICAN_ORIGIN_RUNLOCATION=/tmp/pelican-test/stat_test/xrootdRunLocation
+
+export PELICAN_CONFIGDIR=/tmp/pelican-test/stat_test
+export PELICAN_REGISTRY_DBLOCATION=/tmp/pelican-test/stat_test/test-registry.sql
+export PELICAN_DIRECTOR_DBLOCATION=/tmp/pelican-test/stat_test/test-director.sql
+export PELICAN_OIDC_CLIENTID="sometexthere"
+export PELICAN_OIDC_CLIENTSECRETFILE=/tmp/pelican-test/stat_test/oidc-secret
+echo "Placeholder OIDC secret" > /tmp/pelican-test/stat_test/oidc-secret
+
+export PELICAN_ORIGIN_ENABLEDIRECTREADS=true
+export PELICAN_ORIGIN_FEDERATIONPREFIX="/test"
+export PELICAN_ORIGIN_STORAGEPREFIX="/tmp/pelican-test/stat_test/origin"
+export PELICAN_ORIGIN_ENABLEPUBLICREADS=true
+export PELICAN_DIRECTOR_STATTIMEOUT=1s
+export PELICAN_LOGGING_LEVEL=debug
+
+# Function to cleanup after test ends
+cleanup() {
+    local pid=$1  # Get the PID from the function argument
+    echo "Cleaning up..."
+    if [ ! -z "$pid" ]; then
+    echo "Sending SIGINT to PID $pid"
+    kill -SIGINT "$pid"
+    else
+    echo "No PID provided for cleanup."
+    fi
+
+    # Clean up temporary files
+    rm -rf /tmp/pelican-test/stat_test
+
+    unset PELICAN_CONFIGDIR
+    unset PELICAN_FEDERATION_DIRECTORURL
+    unset PELICAN_FEDERATION_REGISTRYURL
+    unset PELICAN_TLSSKIPVERIFY
+    unset PELICAN_REGISTRY_DBLOCATION
+    unset PELICAN_DIRECTOR_DBLOCATION
+    unset PELICAN_SERVER_ENABLEUI
+    unset PELICAN_OIDC_CLIENTID
+    unset PELICAN_OIDC_CLIENTSECRETFILE
+    unset PELICAN_ORIGIN_ENABLEDIRECTREADS
+    unset PELICAN_ORIGIN_ENABLEPUBLICREADS
+    unset PELICAN_ORIGIN_RUNLOCATION
+    unset PELICAN_ORIGIN_FEDERATION_PREFIX
+    unset PELICAN_ORIGIN_STORAGEPREFIX
+    unset PELICAN_DIRECTOR_STATTIMEOUT
+    unset PELICAN_LOGGING_LEVEL
+}
+
+echo "This is some random content in the random file" > /tmp/pelican-test/stat_test/origin/input.txt
+
+# Prepare token for calling stat
+TOKEN=$(./pelican origin token create --audience "https://wlcg.cern.ch/jwt/v1/any" --issuer "https://`hostname`:8444" --scope "web_ui.access" --subject "bar" --lifetime 3600 --private-key /tmp/pelican-test/stat_test/issuer.jwk)
+
+# Run federation in the background
+federationServe="./pelican serve --module director --module registry --module origin"
+$federationServe &
+pid_federationServe=$!
+
+# Setup trap with the PID as an argument to the cleanup function
+trap 'cleanup $pid_federationServe' EXIT
+
+# Give the federation time to spin up:
+API_URL="https://$HOSTNAME:8444/api/v1.0/health"
+DESIRED_RESPONSE="200"
+
+# Function to check if the response indicates all servers are running
+check_response() {
+    date
+    RESPONSE=$(curl -m 10 -k -s -I -X GET "$API_URL" --write-out "%{http_code}" --output /dev/null \
+                 -H "Content-Type: application/json") \
+
+    # Check if the response matches the desired output
+    if echo "$RESPONSE" | grep -q "$DESIRED_RESPONSE"; then
+        echo "Desired response received: $RESPONSE"
+        return 0
+    else
+        echo "Waiting for desired response..."
+        return 1
+    fi
+}
+
+# We don't want to do this loop for too long, indicates there is an error
+TOTAL_SLEEP_TIME=0
+
+while check_response; [ $? -ne 0 ]
+do
+    sleep .5
+    TOTAL_SLEEP_TIME=$((TOTAL_SLEEP_TIME + 1))
+
+    # Break loop if we sleep for more than 10 seconds
+    if [ "$TOTAL_SLEEP_TIME" -gt 20 ]; then
+        echo "Total sleep time exceeded, exiting..."
+        echo "TEST FAILED"
+        exit 1
+    fi
+done
+
+STAT_URL="https://$HOSTNAME:8444/api/v1.0/director_ui/servers/origins/stat/test/input.txt"
+
+RESPONSE=$(curl -k -H "Cookie: login=$TOKEN" -H "Content-Type: application/json" "$STAT_URL")
+
+if echo "$RESPONSE" | grep -q "\"status\":\"success\""; then
+    echo "Desired response received: $RESPONSE"
+    echo "Test Succeeded"
+    exit 0
+else
+    echo "Stat response returns error: $RESPONSE"
+    echo "Test Failed"
+    exit 1
+fi
